043d0009f8d5eda665d851930a5f99715bb317a4
ZZR-china <2467254599@qq.com>
2017-07-07 11:16:12 +0800

    scroll view add

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,7 +12,47 @@ Page({
     indicatorDots: true,
     autoplay: false,
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    show: [{
+      type: '国内长线',
+      imgurl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      discount: '9.7',
+      title: '兰州、青海湖、茶卡盐湖、祁连、张掖丹霞、嘉峪关、敦煌、莫高窟、鸣沙山双飞7日游',
+      reason: '推荐理由】 【设计理念】 ★ 选择甘肃和青海两省精华景点，集湖泊、草原、沙漠、遗址于一线； ★ 兰州起止，一车一导、汽车去硬卧回，坐车不走回头路，行程更舒适； ★ 硬卧可升级兰新高铁，乘坐最美高铁线兰新高铁',
+      feature: ['最热', '优惠'],
+      originalPrice: '18000',
+      realPrice: '14300'
+    }, {
+      type: '国内中线',
+      imgurl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      discount: '9.7',
+      title: '兰州、青海湖、茶卡盐湖、祁连、张掖丹霞、嘉峪关、敦煌、莫高窟、鸣沙山双飞7日游',
+      reason: '推荐理由】 【设计理念】 ★ 选择甘肃和青海两省精华景点，集湖泊、草原、沙漠、遗址于一线； ★ 兰州起止，一车一导、汽车去硬卧回，坐车不走回头路，行程更舒适； ★ 硬卧可升级兰新高铁，乘坐最美高铁线兰新高铁',
+      feature: ['最热', '优惠'],
+      originalPrice: '18000',
+      realPrice: '14300'
+    }, {
+      type: '周边短线',
+      imgurl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      discount: '9.7',
+      title: '兰州、青海湖、茶卡盐湖、祁连、张掖丹霞、嘉峪关、敦煌、莫高窟、鸣沙山双飞7日游',
+      reason: '推荐理由】 【设计理念】 ★ 选择甘肃和青海两省精华景点，集湖泊、草原、沙漠、遗址于一线； ★ 兰州起止，一车一导、汽车去硬卧回，坐车不走回头路，行程更舒适； ★ 硬卧可升级兰新高铁，乘坐最美高铁线兰新高铁',
+      feature: ['最热', '优惠'],
+      originalPrice: '18000',
+      realPrice: '14300'
+    }, {
+      type: '自驾游',
+      imgurl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      discount: '9.7',
+      title: '兰州、青海湖、茶卡盐湖、祁连、张掖丹霞、嘉峪关、敦煌、莫高窟、鸣沙山双飞7日游',
+      reason: '推荐理由】 【设计理念】 ★ 选择甘肃和青海两省精华景点，集湖泊、草原、沙漠、遗址于一线； ★ 兰州起止，一车一导、汽车去硬卧回，坐车不走回头路，行程更舒适； ★ 硬卧可升级兰新高铁，乘坐最美高铁线兰新高铁',
+      feature: ['最热', '优惠'],
+      originalPrice: '18000',
+      realPrice: '14300'
+    }]
+  },
+  scroll: function(e) {
+    console.log(e)
   },
   onLoad: function () {
     app.getUserInfo(user => {
